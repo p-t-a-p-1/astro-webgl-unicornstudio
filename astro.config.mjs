273f97d8d17983fce9a30ed0eb1db19ca9597f84
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2024-12-03 13:59:56 +0900

    update GitHub hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://p-t-a-p-1.github.io",
+  base: "/astro-webgl-unicornstudio",
+});
